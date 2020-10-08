d05f254094e7c06dea34b15b6491c4d3eb4bc5d2
Dvader10 <daniel.pascoalato@uol.com.br>
2020-10-07 21:48:34 -0300

    Adding Address Unit test

---
+++ src/test/java/br/com/docket/registry/model/AddressTest.java
@@ -0,0 +1,29 @@
+package br.com.docket.registry.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressTest {
+
+    @Test
+    public void whenStreetAddressIsBlankShouldThrowIllegalArgumentException(){
+        assertThatIllegalArgumentException().isThrownBy(()-> new Address("","237649", "São Paulo", "SP"));
+    }
+
+    @Test
+    public void whenPostalCodeIsBlankShouldThrowIllegalArgumentException(){
+        assertThatIllegalArgumentException().isThrownBy(()-> new Address("Av Paulista 1000","", "São Paulo", "SP"));
+    }
+
+    @Test
+    public void whenCityIsBlankShouldThrowIllegalArgumentException(){
+        assertThatIllegalArgumentException().isThrownBy(()-> new Address("Av Paulista 1000","237649", "", "SP"));
+    }
+
+    @Test
+    public void whenStateIsBlankShouldThrowIllegalArgumentException(){
+        assertThatIllegalArgumentException().isThrownBy(()-> new Address("Av Paulista 1000","237649", "São Paulo", ""));
+    }
+}
