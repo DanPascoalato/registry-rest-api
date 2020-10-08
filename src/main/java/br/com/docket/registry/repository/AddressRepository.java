3f8b500f8ece321ecf5581e77b3e6b185e688b4c
Dvader10 <daniel.pascoalato@uol.com.br>
2020-10-08 14:08:20 -0300

    Creating Address Repository

---
+++ src/main/java/br/com/docket/registry/repository/AddressRepository.java
@@ -0,0 +1,15 @@
+package br.com.docket.registry.repository;
+
+
+import br.com.docket.registry.model.Address;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional(value = Transactional.TxType.MANDATORY)
+public interface AddressRepository extends CrudRepository<Address, Integer> {
+
+}
