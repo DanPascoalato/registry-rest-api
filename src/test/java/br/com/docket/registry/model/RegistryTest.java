07bbd8893c82f20847e66c5eb437e6bccbae3b78
Dvader10 <daniel.pascoalato@uol.com.br>
2020-10-07 21:57:47 -0300

    Adding Registry Unit test

---
+++ src/test/java/br/com/docket/registry/model/RegistryTest.java
@@ -0,0 +1,27 @@
+package br.com.docket.registry.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class RegistryTest {
+
+    private Address address = new Address("Av Paulista 1000", "234524", "São Paulo", "SP");
+    private Certificate cert = new Certificate(1, "Certi de Nascimento");
+
+    @Test
+    public void whenNameIsBlankShouldThrowIllegalArgumentException(){
+        assertThatIllegalArgumentException().isThrownBy(()-> new Registry("", address, cert));
+    }
+
+    @Test
+    public void whenAddressIsNullShouldThrowIllegalArgumentException(){
+        assertThatIllegalArgumentException().isThrownBy(()-> new Registry("Cartorio de notas", null, cert));
+    }
+
+    @Test
+    public void whenCertificateIsNullShouldThrowIllegalArgumentException(){
+        assertThatIllegalArgumentException().isThrownBy(()-> new Registry("Cartorio de notas", address, null));
+    }
+
+}
