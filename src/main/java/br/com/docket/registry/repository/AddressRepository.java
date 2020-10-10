4f466d11a46e5cf54878126b79e93c7f0d84d64a
Dvader10 <daniel.pascoalato@uol.com.br>
2020-10-10 20:06:00 -0300

    Adding Optional to AddressRepository

--- src/main/java/br/com/docket/registry/repository/AddressRepository.java
+++ src/main/java/br/com/docket/registry/repository/AddressRepository.java
@@ -2,14 +2,17 @@ package br.com.docket.registry.repository;
 
 
 import br.com.docket.registry.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Transactional(value = Transactional.TxType.MANDATORY)
-public interface AddressRepository extends CrudRepository<Address, Long> {
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
+    public Optional<Address> findByStreetAddress(String streetAddress);
 
 }
