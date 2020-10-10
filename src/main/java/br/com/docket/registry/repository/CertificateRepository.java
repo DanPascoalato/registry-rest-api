9eed7de743b845481714b9edb06eeb28ab92209f
Dvader10 <daniel.pascoalato@uol.com.br>
2020-10-10 20:06:26 -0300

    Adding Optional to CertificateRepository

--- src/main/java/br/com/docket/registry/repository/CertificateRepository.java
+++ src/main/java/br/com/docket/registry/repository/CertificateRepository.java
@@ -1,13 +1,17 @@
 package br.com.docket.registry.repository;
 
 import br.com.docket.registry.model.Certificate;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Repository
 @Transactional(value = Transactional.TxType.MANDATORY)
-public interface CertificateRepository extends CrudRepository<Certificate, Long> {
+public interface CertificateRepository extends JpaRepository<Certificate, Long> {
+
+    public Optional<Certificate> findByName(String certificateName);
 
 }
