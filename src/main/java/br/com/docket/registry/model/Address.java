dbda7dfa5f7a28a2aa9da12c9735536971716001
Dvader10 <daniel.pascoalato@uol.com.br>
2020-10-11 00:20:33 -0300

    Adding streetNumber column and creating update method

--- src/main/java/br/com/docket/registry/model/Address.java
+++ src/main/java/br/com/docket/registry/model/Address.java
@@ -15,9 +15,13 @@ public class Address {
     @JsonProperty
     private Long id;
 
-    @Column(name = "streetAddress")
-    @JsonProperty("street")
+    @Column(name = "streetName")
+    @JsonProperty("streetName")
     private String streetAddress;
+
+    @Column(name = "streetNumber")
+    @JsonProperty("streetNumber")
+    private String number;
 
     @Column(name = "postalCode")
     @JsonProperty("postalCode")
@@ -32,43 +36,68 @@ public class Address {
     private String state;
 
     private Address() {
-
     }
 
-    public Address(String streetAddress, String postalCode, String city, String state){
+    public Address(Long id, String streetAddress, String number, String postalCode, String city, String state) {
+        setId(id);
         setStreetAddress(streetAddress);
+        setNumber(number);
         setPostalCode(postalCode);
         setCity(city);
         setState(state);
     }
 
-    private void setStreetAddress(String streetAddress) {
-        if(streetAddress.isBlank()){
+    public Address updateWith(Address addr) {
+        setStreetAddress(addr.getStreetAddress());
+        setPostalCode(addr.getPostalCode());
+        setCity(addr.getCity());
+        setState(addr.getState());
+        setNumber(addr.getNumber());
+        return this;
+    }
+
+
+    private void setId(long id) {
+        if (id <= 0) {
             throw new IllegalArgumentException();
+        }
+        this.id = id;
+    }
+
+    private void setStreetAddress(String streetAddress) {
+        if (streetAddress == null || streetAddress.isBlank()) {
+            throw new IllegalArgumentException("Street Address cannot be null or empty");
         }
         this.streetAddress = streetAddress.trim();
     }
 
+    private void setNumber(String number) {
+        if (number == null || number.isBlank()) {
+            this.number = "SN";
+        } else {
+            this.number = number;
+        }
+    }
+
     private void setPostalCode(String postalCode) {
-        if(postalCode.isBlank()){
-            throw new IllegalArgumentException();
+        if (postalCode == null || postalCode.isBlank()) {
+            throw new IllegalArgumentException("Postal Code cannot be null or empty");
         }
         this.postalCode = postalCode.trim();
     }
 
     private void setCity(String city) {
-        if(city.isBlank()){
-            throw new IllegalArgumentException();
+        if (city == null || city.isBlank()) {
+            throw new IllegalArgumentException("City cannot be null or empty");
         }
         this.city = city.trim();
     }
 
     private void setState(String state) {
-        if(state.isBlank()){
-            throw new IllegalArgumentException();
+        if (state == null || state.isBlank()) {
+            throw new IllegalArgumentException("State cannot be null or empty");
         }
         this.state = state.trim();
     }
-
 
 }
